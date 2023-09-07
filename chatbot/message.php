0fb46ed9eb03e3429cf15974e9aedc537a69bf20
ahmedanas37 <ahmedanas37@gmail.com>
2023-09-08 02:35:10 +0500

    N/A

--- chatbot/message.php
+++ chatbot/message.php
@@ -1,24 +1,29 @@
 <!-- Created By CampCodes -->
 <?php
-// connecting to database
+// connecting to the database
 $conn = mysqli_connect("localhost", "root", "", "project") or die("Database Error");
 
-// getting user message through ajax
+// getting user message through AJAX
 $getMesg = mysqli_real_escape_string($conn, $_POST['text']);
 
-//checking user query to database query
-$check_data = "SELECT replies FROM chatbot WHERE queries LIKE '%$getMesg%'";
-$run_query = mysqli_query($conn, $check_data) or die("Error");
+// checking user query against database queries using prepared statement
+$check_data = "SELECT replies FROM chatbot WHERE queries = ?";
+$stmt = mysqli_prepare($conn, $check_data);
+mysqli_stmt_bind_param($stmt, "s", $getMesg);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_store_result($stmt);
 
-// if user query matched to database query we'll show the reply otherwise it go to else statement
-if(mysqli_num_rows($run_query) > 0){
-    //fetching replay from the database according to the user query
-    $fetch_data = mysqli_fetch_assoc($run_query);
-    //storing replay to a varible which we'll send to ajax
-    $replay = $fetch_data['replies'];
+// if user query matched to database query, show the reply; otherwise, go to the else statement
+if (mysqli_stmt_num_rows($stmt) > 0) {
+    // fetching reply from the database according to the user query
+    mysqli_stmt_bind_result($stmt, $replay);
+    mysqli_stmt_fetch($stmt);
     echo $replay;
-}else{
-    echo "Sorry can't be able to understand you!";
+} else {
+    echo "Sorry, can't be able to understand you!";
 }
 
+// close the prepared statement and database connection
+mysqli_stmt_close($stmt);
+mysqli_close($conn);
 ?>
